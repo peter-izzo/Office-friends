ae021e0dd534bb7062a0393ea431ae740c4f2627
Peter Izzo <peter.izzo528@gmail.com>
2020-06-26 00:47:06 -0400

    Updates

---
+++ precache-manifest.5e66e093c85f0a242f3983bf2e742fff.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "831dfc3943ccfded6b7e6d1689262065",
+    "url": "/Office-friends/index.html"
+  },
+  {
+    "revision": "fbbaeac22e7ea9bdd006",
+    "url": "/Office-friends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ca165adad9b47c8a7012",
+    "url": "/Office-friends/static/css/main.f351a2b8.chunk.css"
+  },
+  {
+    "revision": "fbbaeac22e7ea9bdd006",
+    "url": "/Office-friends/static/js/2.19988c72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Office-friends/static/js/2.19988c72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca165adad9b47c8a7012",
+    "url": "/Office-friends/static/js/main.85250843.chunk.js"
+  },
+  {
+    "revision": "aeab0d8c5ae1502e86dc",
+    "url": "/Office-friends/static/js/runtime-main.18d2dc55.js"
+  },
+  {
+    "revision": "357a79e5befe5adefeb1d12050ed95fc",
+    "url": "/Office-friends/static/media/1.357a79e5.png"
+  },
+  {
+    "revision": "b6cfbe712f87760d4328ad21e2acf620",
+    "url": "/Office-friends/static/media/10.b6cfbe71.png"
+  },
+  {
+    "revision": "423ad930b34c83778d74bf6f8d81d6cd",
+    "url": "/Office-friends/static/media/11.423ad930.png"
+  },
+  {
+    "revision": "44c483e6a25a0a15c03322b81361a1fb",
+    "url": "/Office-friends/static/media/12.44c483e6.png"
+  },
+  {
+    "revision": "c0576f5615e789827aa8e83dbee75dfd",
+    "url": "/Office-friends/static/media/13.c0576f56.png"
+  },
+  {
+    "revision": "a9e121cf0b1c77b36600f28a3c9b8df8",
+    "url": "/Office-friends/static/media/14.a9e121cf.png"
+  },
+  {
+    "revision": "b5c395737091fa0e1a3de0f29f60eceb",
+    "url": "/Office-friends/static/media/15.b5c39573.png"
+  },
+  {
+    "revision": "b9eb0254c4980e9ed250095839dd2212",
+    "url": "/Office-friends/static/media/16.b9eb0254.png"
+  },
+  {
+    "revision": "cb09223928a81c2c6b2d4045b0b223f8",
+    "url": "/Office-friends/static/media/17.cb092239.png"
+  },
+  {
+    "revision": "d4964000742858185a3ace8acb2815dd",
+    "url": "/Office-friends/static/media/18.d4964000.png"
+  },
+  {
+    "revision": "5d351a4b54682bef775895cd49398460",
+    "url": "/Office-friends/static/media/2.5d351a4b.png"
+  },
+  {
+    "revision": "919174513950eee767127e2edc943c98",
+    "url": "/Office-friends/static/media/3.91917451.png"
+  },
+  {
+    "revision": "079af54b45940ba5e8d2e9a1aa279234",
+    "url": "/Office-friends/static/media/4.079af54b.png"
+  },
+  {
+    "revision": "7150e29d029242bbc67662991256fe8e",
+    "url": "/Office-friends/static/media/5.7150e29d.png"
+  },
+  {
+    "revision": "7c7ecf01c6869ce5dd4d76a77defbcb2",
+    "url": "/Office-friends/static/media/6.7c7ecf01.png"
+  },
+  {
+    "revision": "816767b305d19e8c25e92d854f697399",
+    "url": "/Office-friends/static/media/7.816767b3.png"
+  },
+  {
+    "revision": "b2a160c3dee24a3616cace7544d9cb5d",
+    "url": "/Office-friends/static/media/8.b2a160c3.png"
+  },
+  {
+    "revision": "572faa272858d16ba2788de9adbde034",
+    "url": "/Office-friends/static/media/9.572faa27.png"
+  }
+]);
